aa647ec04185997cf606fc5ec09543176cda6a7c
bonghyerim <130967304+bonghyerim@users.noreply.github.com>
2023-06-19 09:40:39 +0900

    2023

--- app.py
+++ app.py
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-# app.py
-
-from flask import Flask
-from flask_restful import Api
-from resources.recipe import RecipeListResource
-
-# API 서버를 구축하기 위한 기본 구조
-app = Flask(__name__)
-
-# restfulAPI 생성
-api = Api(app)
-
-# 경로와 리소스(api코드) 연결
-api.add_resource(RecipeListResource, '/recipes')
-
-if __name__ == '__main__' :
-=======
 # conda create -n lambda_app python=3.10
 # conda env remove -n lambda_app
 # lambda_app 가상환경에서 작업. 
