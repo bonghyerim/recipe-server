252bef94e4566ca60e1fcb5a7a4efb99809f0fc5
bonghyerim <130967304+bonghyerim@users.noreply.github.com>
2023-06-19 09:41:16 +0900

    2023

--- mysql_connection.py
+++ mysql_connection.py
@@ -1,14 +1,4 @@
-<<<<<<< HEAD
-import mysql.connector
-from config import Config
 
-def get_connection() :
-    connection = mysql.connector.connect(
-        host='database-1.c56mxvfytstp.ap-northeast-2.rds.amazonaws.com', # 호스트는 사용자의 호스트 입력
-        database='recipe', # 예시에 쓸 DB recipe
-        user='recipe_db_user', # 관리 권한을 가진 'recipe_user1234'로 접속
-        password='1234' )
-=======
 # 데이터베이스 연결 전용 파일
 import mysql.connector
 from config import Config
@@ -20,5 +10,3 @@ def get_connection() :
         user = Config.DB_USER,
         password = Config.DB_PASSWORD
     )
->>>>>>> f0ec4aba87431c4d3a20425dd4309e8857bea939
-    return connection
